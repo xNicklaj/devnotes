6a8d29cb931b26d2d82e9dea2a8c6dcfc342fede
xNicklaj <simonearonica@protonmail.ch>
2025-03-16 20:46:17 +0100

    Fixed typo in quartz config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Nicklaj's Technotes,
+    pageTitle: "Nicklaj's Technotes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
